e9ad6ca7ac89094d70fd4a744a44562d31ea4c3d
nizent <43146103+nizent@users.noreply.github.com>
2021-07-08 23:40:25 -0400

    Delete src/main/java directory

--- src/main/java/cl/uchile/dcc/scrabble/models/operation/constantFactory/ConstantFactory.java
+++
@@ -1,7 +0,0 @@
-package cl.uchile.dcc.scrabble.models.operation.constantFactory;
-
-/**
- * This interface represents a Constant Factory. Uses Factory Pattern
- */
-public interface ConstantFactory {
-}
--- src/main/java/cl/uchile/dcc/scrabble/models/operation/logical/LogicalOperation.java
+++
@@ -1,9 +0,0 @@
-package cl.uchile.dcc.scrabble.models.operation.logical;
-
-import cl.uchile.dcc.scrabble.models.operation.Operation;
-
-/**
- * This interface represents a logical operation as and, or & negation
- */
-public interface LogicalOperation extends Operation {
-}
